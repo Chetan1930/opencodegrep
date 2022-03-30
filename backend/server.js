e852ed77b73825622e17857be4d372cb8254419d
nisabmohd <nisabmohd.com>
2022-03-30 20:48:13 +0530

    run button added in client, Post form encoded text backend

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-// server
+const express = require('express')
+var cors = require('cors')
+var bodyParser = require('body-parser')
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.post('/run', (req, res) => {
+    console.log(req.body);
+    res.send("hello")
+})
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log("listening on port 5000");
+})
+
--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import run from './assets/runbtn.png'
 export const Navbar = () => {
     return (
-            <nav class="navbar navbar-light" style={{backgroundColor: 'rgb(36 38 38)'}}>
-                <div class="container-fluid">
-                    <a href="/" class="navbar-brand" style={{color:'white'}}>Online Compiler</a>
+        <nav class="navbar navbar-light" style={{ backgroundColor: 'rgb(36 38 38)' }}>
+            <div class="container-fluid">
+                <a href="/" class="navbar-brand" style={{ color: 'white' }}>Online Compiler</a>
+                <div style={{display:'flex',flexDirection:'row'}}>
+                    <button style={{backgroundColor:'transparent',outline:'none',border:'none'}}><img src={run} style={{width:'34px'}} alt="" /></button>
                     <select class="form-select mx-4" style={{ width: '12vw' }} aria-label="Default select example">
                         <option value="cpp">C++</option>
                         <option value="java">Java</option>
@@ -13,6 +15,7 @@ export const Navbar = () => {
                         <option value="c">C</option>
                     </select>
                 </div>
-            </nav>
+            </div>
+        </nav>
     )
 }
