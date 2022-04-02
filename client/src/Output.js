68a69994ab3f6fec7a7d34fb125d1ed954e43e66
nisabmohd <nisabmohd.com>
2022-04-02 11:58:02 +0530

    output board fix

--- client/src/Output.js
+++ client/src/Output.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Output = (props) => {
     return (
-        <div className="output" style={{ display: 'flex', flexDirection: 'column', textAlign: 'left', paddingLeft: '0.8vw', backgroundColor: props.bgcol, marginTop: '-8px', paddingTop: '40px', overflowY: 'scroll', position: 'relative', height: '22.5vh', fontSize: '0.98em', borderTop: `0.1px solid ${props.border}`, color: props.col }}>
+        <div className="output" style={{ display: 'flex', flexDirection: 'column', textAlign: 'left', paddingLeft: '0.8vw', backgroundColor: props.bgcol, marginTop: '-8px', paddingTop: '40px', overflowY: 'scroll', position: 'relative', height: '30vh', fontSize: '0.98em', borderTop: `0.1px solid ${props.border}`, color: props.col }}>
             <h6 style={{ color: props.col, zIndex: '99', position: 'absolute', top: '15px', fontSize: '15.25px' }}>Output </h6>
             <div className="output" style={{ color: 'white', display: 'flex', flexDirection: 'column' }}>
                 {(props.op[0]) ?
--- client/src/TextEditor.jsx
+++ client/src/TextEditor.jsx
@@ -8,7 +8,7 @@ export const TextEditor = (props) => {
     return (
 
         // <textarea style={{ border: 'none', outline: 'none', backgroundColor: 'black', width: '100%', height: '70vh', color: 'white', textDecoration: 'none', paddingLeft: '0.8vw', paddingTop: '19px', fontSize: '0.85em', overflowX: 'scroll', overfowY: 'scroll' }} spellCheck="false" onChange={(e) => handleCode(e)} value={props.c}>  </textarea>
-        <div className="textarea" style={{height:'72vh',overflowY:'scroll',backgroundColor: props.bgcol}}>
+        <div className="textarea" style={{height:'65vh',overflowY:'scroll',backgroundColor: props.bgcol}}>
             <CodeEditor
                 value={props.c}
                 language={props.lang}
