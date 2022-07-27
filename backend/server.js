e56ae72c3f5cc6e719cc6e428bd23b27b4d225ff
nisabmohd <nisabmohd@gmail.com>
2022-07-27 21:58:41 +0530

    added auth routes at backend and added modal for login front end

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,18 @@ const { runjava } = require('./runJava')
 const { runpy } = require('./runPy')
 const { runc } = require('./runC')
 const { deleteFile } = require('./deleteFile')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const { userModel } = require('./models/user')
+const bcrypt = require("bcrypt");
+var uniqid = require('uniqid');
 
+
+mongoose.connect(process.env.mongodb).then(() => {
+    console.log("connected");
+}).catch(err => {
+    console.log(err);
+})
 
 const app = express()
 app.use(express.json())
@@ -20,14 +31,14 @@ const io = new Server(server, { cors: { origin: "*" } })
 
 io.on('connection', (socket) => {
     console.log(`a user connected with id :${socket.id}`);
-    socket.on('getcode',(code)=>{
-        if(code.roomid)
-        socket.broadcast.emit(`sendcode${code.roomid}`, {code:code.code,lang:code.lang});
+    socket.on('getcode', (code) => {
+        if (code.roomid)
+            socket.broadcast.emit(`sendcode${code.roomid}`, { code: code.code, lang: code.lang });
     })
-    socket.on('disconnect',()=>{
+    socket.on('disconnect', () => {
         console.log(`a user disconnected with id: ${socket.id}`);
     })
-  });
+});
 
 app.post('/run', async (req, res) => {
     const inDate = new Date()
@@ -64,6 +75,40 @@ app.post('/run', async (req, res) => {
         deleteFile(inputFile)
         res.send(out);
     }
+})
+
+app.post('/auth/login', async (req, res) => {
+    try {
+        const user = await userModel.findOne({ email: req.body.email })
+        const validPassword = await bcrypt.compare(req.body.password, user.password);
+        if (validPassword)
+            res.status(200).send(await userModel.findOne({ email: req.body.email }, { password: 0 }))
+        else res.status(400).send({ message: "wrong password" });
+    } catch (err) {
+        if (err) {
+            res.status(400).send("User doesn't exist")
+        }
+    }
+})
+
+app.post('/auth/signup', async(req, res) => {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const createuser = new userModel({
+            uid: uniqid(),
+            email: req.body.email,
+            password: await bcrypt.hash(req.body.password, salt)
+        })
+        await createuser.save();
+        res.status(200).send(await userModel.findOne({ email: req.body.email }, { password: 0,email:0,_id:0 }))
+    }
+    catch (err) {
+        res.status(400).send(err)
+    }
+})
+
+app.post('/savecode', (req, res) => {
+    //body has code + uid + filename +time
 })
 
 server.listen(process.env.PORT || 8000, () => {
